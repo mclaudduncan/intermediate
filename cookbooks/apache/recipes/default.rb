edae174c3ffc4a4febfe5aa1b923a3dc55c42b99
david <david@curso.com>
2019-06-25 19:56:55 +0000

    Añadido test de integración y preimera receta

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+package 'httpd'
+
+file '/var/www/html/index.html' do
+   content '<h1>Welcome Home!</h1>'
+end
+
+service 'httpd' do
+   action [:enable, :start]
+end
+
