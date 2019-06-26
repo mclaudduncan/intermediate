93463b4d04da0bb58e47676ec609a7cf17819ce4
maclaud <david@cursochef.com>
2019-06-26 18:51:51 +0000

    Añadidos atributos a las recetas

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default['apache']['package_name'] = 'httpd'
+default['apache']['service_name'] = 'httpd'
+default['apache']['default_index_html'] = '/var/www/html/index.html'
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,13 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-package 'httpd'
+#package 'httpd'
+package node['apache']['package_name']
 
-file '/var/www/html/index.html' do
+file node['apache']['default_index_html'] do
    content '<h1>Welcome Home!</h1>'
 end
 
-service 'httpd' do
+service node['apache']['service_name'] do
    action [:enable, :start]
 end
 
