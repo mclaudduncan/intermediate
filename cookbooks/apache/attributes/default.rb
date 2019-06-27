ef3c74947999633e9adb4aabc1480205c4c2849e
mclaud <david@cursochef.com>
2019-06-27 18:39:08 +0200

    Refactorizado multiplataforma Centos 7 Ubunto 18.04

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,3 +1,10 @@
-default['apache']['package_name'] = 'httpd'
-default['apache']['service_name'] = 'httpd'
+case node['platform']
+when 'ubuntu'
+    default['apache']['package_name'] = 'apache2'
+    default['apache']['service_name'] = 'apache2'
+else
+    default['apache']['package_name'] = 'httpd'
+    default['apache']['service_name'] = 'httpd'
+end
+
 default['apache']['default_index_html'] = '/var/www/html/index.html'
